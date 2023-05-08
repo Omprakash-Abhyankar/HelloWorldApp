2448046407421813ae2bed284d064244099d5ae1
Omprakash Abhyankar <omabhyankar03@gmail.com>
2023-05-08 15:08:29 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace HelloWorldApp
+{
+    internal class Program
+    {
+        //Enrty Point Of an C# Programme.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World!");
+            //if we did not write the message is not show in console..
+            // 
+            Console.ReadKey();
+        }
+    }
+}
